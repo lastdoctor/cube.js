d2424d822ca7be58a3eebb759b0e9365846a7bfd
Hassan Khan <hassan@cube.dev>
2021-05-16 00:36:27 +0100

    docs(cloud): move Cube IDE to Developer Tools to match OSS docs

--- docs/src/components/Layout/MainMenu.tsx
+++ docs/src/components/Layout/MainMenu.tsx
@@ -19,7 +19,7 @@ import { layout } from '../../theme';
 const menuOrderCloud = [
   'Cube Cloud Getting Started',
   'Configuring Cube Cloud',
-  'Cube IDE',
+  'Developer Tools',
   'Deploys',
   'Inspecting Queries'
 ];
